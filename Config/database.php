6937d18920668ec154400795d16fe02a708958a8
manitcs1982 <62586698+manitcs1982@users.noreply.github.com>
2022-09-15 16:43:39 +0530

    Update database.php

--- Config/database.php
+++ Config/database.php
@@ -32,7 +32,6 @@ class Database{
 	PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
 	PDO::MYSQL_ATTR_SSL_CA => true,
 	PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
-					
 )
 							);
 	
@@ -57,7 +56,7 @@ class Database{
 	// DB connection for Rate card master database
 	public function GetRateCardConnection(){		
 		try{
-			$this->conn = new PDO("mysql:host=" . $this->servername . ";dbname=" . $this->databaseRateCard, $this->username, $this->password);
+			$this->conn = new PDO("mysql:host=" . $this->servername . ";dbname=" . $this->databaseRateCard, $this->username, $this->password,$this->db_info['opts']);
 			$this->conn->exec("set names utf8");			
 		}catch(PDOException $exception){
 			echo "Connection error: " . $exception->getMessage();
@@ -74,7 +73,7 @@ class Database{
 	// DB connection for Address database
 	public function GetAddressConnection(){		
 		try{
-			$this->conn = new PDO("mysql:host=" . $this->servername . ";dbname=" . $this->databaseAddress, $this->username, $this->password);
+			$this->conn = new PDO("mysql:host=" . $this->servername . ";dbname=" . $this->databaseAddress, $this->username, $this->password,$this->db_info['opts']);
 			$this->conn->exec("set names utf8");			
 		}catch(PDOException $exception){
 			echo "Connection error: " . $exception->getMessage();
@@ -91,7 +90,7 @@ class Database{
 	// DB connection for Pincode Master database
 	public function GetPincodeMasterConnection(){		
 		try{
-			$this->conn = new PDO("mysql:host=" . $this->servername . ";dbname=" . $this->databasePincodeMaster, $this->username, $this->password);
+			$this->conn = new PDO("mysql:host=" . $this->servername . ";dbname=" . $this->databasePincodeMaster, $this->username, $this->password,$this->db_info['opts']);
 			$this->conn->exec("set names utf8");			
 		}catch(PDOException $exception){
 			echo "Connection error: " . $exception->getMessage();
@@ -108,7 +107,7 @@ class Database{
 	// DB connection for Service database
 	public function GetServiceConnection(){		
 		try{
-			$this->conn = new PDO("mysql:host=" . $this->servername . ";dbname=" . $this->databaseService, $this->username, $this->password);
+			$this->conn = new PDO("mysql:host=" . $this->servername . ";dbname=" . $this->databaseService, $this->username, $this->password,$this->db_info['opts']);
 			$this->conn->exec("set names utf8");			
 		}catch(PDOException $exception){
 			echo "Connection error: " . $exception->getMessage();
@@ -125,7 +124,7 @@ class Database{
 	// DB connection for Service database
 	public function GetVendorConnection(){		
 		try{
-			$this->conn = new PDO("mysql:host=" . $this->servername . ";dbname=" . $this->databaseVendor, $this->username, $this->password);
+			$this->conn = new PDO("mysql:host=" . $this->servername . ";dbname=" . $this->databaseVendor, $this->username, $this->password,$this->db_info['opts']);
 			$this->conn->exec("set names utf8");			
 		}catch(PDOException $exception){
 			echo "Connection error: " . $exception->getMessage();
@@ -142,7 +141,7 @@ class Database{
 	// DB connection for Schedule database
 	public function GetScheduleConnection(){		
 		try{
-			$this->conn = new PDO("mysql:host=" . $this->servername . ";dbname=" . $this->databaseSchedule, $this->username, $this->password);
+			$this->conn = new PDO("mysql:host=" . $this->servername . ";dbname=" . $this->databaseSchedule, $this->username, $this->password,$this->db_info['opts']);
 			$this->conn->exec("set names utf8");			
 		}catch(PDOException $exception){
 			echo "Connection error: " . $exception->getMessage();
@@ -159,7 +158,7 @@ class Database{
 	// DB connection for Customer Product database
 	public function GetCustomerProductConnection(){		
 		try{
-			$this->conn = new PDO("mysql:host=" . $this->servername . ";dbname=" . $this->databaseCustomerProduct, $this->username, $this->password);
+			$this->conn = new PDO("mysql:host=" . $this->servername . ";dbname=" . $this->databaseCustomerProduct, $this->username, $this->password,$this->db_info['opts']);
 			$this->conn->exec("set names utf8");			
 		}catch(PDOException $exception){
 			echo "Connection error: " . $exception->getMessage();
@@ -176,7 +175,7 @@ class Database{
 	// DB connection for Product Master database
 	public function GetProductConnection(){		
 		try{
-			$this->conn = new PDO("mysql:host=" . $this->servername . ";dbname=" . $this->databaseProduct, $this->username, $this->password);
+			$this->conn = new PDO("mysql:host=" . $this->servername . ";dbname=" . $this->databaseProduct, $this->username, $this->password,$this->db_info['opts']);
 			$this->conn->exec("set names utf8");			
 		}catch(PDOException $exception){
 			echo "Connection error: " . $exception->getMessage();
@@ -192,7 +191,7 @@ class Database{
 	// DB connection for Logistics database
 	public function GetLogisticsConnection(){		
 		try{
-			$this->conn = new PDO("mysql:host=" . $this->servername . ";dbname=" . $this->databaseLogistics, $this->username, $this->password);
+			$this->conn = new PDO("mysql:host=" . $this->servername . ";dbname=" . $this->databaseLogistics, $this->username, $this->password,$this->db_info['opts']);
 			$this->conn->exec("set names utf8");			
 		}catch(PDOException $exception){
 			echo "Connection error: " . $exception->getMessage();
@@ -210,7 +209,7 @@ class Database{
 	public function GetNotificationMasterConnection()
 	{		
 		try{
-			$this->conn = new PDO("mysql:host=" . $this->servername . ";dbname=" . $this->databaseNotificationMaster, $this->username, $this->password);
+			$this->conn = new PDO("mysql:host=" . $this->servername . ";dbname=" . $this->databaseNotificationMaster, $this->username, $this->password,$this->db_info['opts']);
 			$this->conn->exec("set names utf8");			
 		}catch(PDOException $exception){
 			echo "Connection error: " . $exception->getMessage();
@@ -228,7 +227,7 @@ class Database{
 	public function GetRoleMasterConnection()
 	{		
 		try{
-			$this->conn = new PDO("mysql:host=" . $this->servername . ";dbname=" . $this->databaseRoleMaster, $this->username, $this->password);
+			$this->conn = new PDO("mysql:host=" . $this->servername . ";dbname=" . $this->databaseRoleMaster, $this->username, $this->password,$this->db_info['opts']);
 			$this->conn->exec("set names utf8");			
 		}catch(PDOException $exception){
 			echo "Connection error: " . $exception->getMessage();
@@ -246,7 +245,7 @@ class Database{
 	public function GetChatConnection()
 	{		
 		try{
-			$this->conn = new PDO("mysql:host=" . $this->servername . ";dbname=" . $this->databaseChat, $this->username, $this->password);
+			$this->conn = new PDO("mysql:host=" . $this->servername . ";dbname=" . $this->databaseChat, $this->username, $this->password,$this->db_info['opts']);
 			$this->conn->exec("set names utf8");			
 		}catch(PDOException $exception){
 			echo "Connection error: " . $exception->getMessage();
@@ -266,7 +265,7 @@ class Database{
 	}
 	public function GetConstantValuesConnection(){		
 		try{
-			$this->conn = new PDO("mysql:host=" . $this->servername . ";dbname=" . $this->databaseConstantValues, $this->username, $this->password);
+			$this->conn = new PDO("mysql:host=" . $this->servername . ";dbname=" . $this->databaseConstantValues, $this->username, $this->password,$this->db_info['opts']);
 			$this->conn->exec("set names utf8");			
 		}catch(PDOException $exception){
 			echo "Connection error: " . $exception->getMessage();
@@ -279,8 +278,6 @@ class Database{
 		}
 		return $this->conn;
 	}
-
-
 
 } 
 ?>
