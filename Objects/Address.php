2d35b8056885768423d849c504b27e767501d396
manitcs1982 <62586698+manitcs1982@users.noreply.github.com>
2022-09-19 13:48:39 +0530

    Add files via upload
    Frequently used address functionality

--- Objects/Address.php
+++ Objects/Address.php
@@ -66,7 +66,7 @@ class Address{
 		//Getting Address by customerId
 		public function GetAddressByCustomerId(){			
 			try{
-				$sql = "select * from Address where ADR_CSR_GFK=:customerId and ADR_status=1  order by ADR_srCount"; //
+				$sql = "select * from Address where ADR_CSR_GFK=:customerId and ADR_status=1  order by ADR_srCount desc"; //
 				$result = $this->conn->prepare($sql);
 				$this->customerId=htmlspecialchars(strip_tags($this->customerId));				
 				$result->bindParam(":customerId", $this->customerId);
